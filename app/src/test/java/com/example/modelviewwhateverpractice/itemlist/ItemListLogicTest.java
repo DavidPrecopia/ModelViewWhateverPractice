c41585020854cea20d7516e963c5f2619b27ad7a
David Precopia <35779726+DavidPrecopia@users.noreply.github.com>
2019-06-30 16:32:33 -0400

    Comment explaining Trampoline Scheduler

--- app/src/test/java/com/example/modelviewwhateverpractice/itemlist/ItemListLogicTest.java
+++ app/src/test/java/com/example/modelviewwhateverpractice/itemlist/ItemListLogicTest.java
@@ -47,6 +47,15 @@ public class ItemListLogicTest {
     private ItemListLogic logic;
 
 
+    /**
+     * Trampoline: a Scheduler that queues work on the current thread to be
+     * executed after the current work completes.
+     * Another way to put it: emits result in a sequentially predictable order.
+     *
+     * Because this is a unit test that is running on the JVM,
+     * all operations run on the same thread the tests are running on.
+     * Otherwise, an error in thrown by the Observable.
+     */
     @Before
     public void init() {
         when(schedulerProvider.io()).thenReturn(Schedulers.trampoline());
